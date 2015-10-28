003f19932b48dbda04f400765c486d38822dc8c2
ALRW <awerner1@googlemail.com>
2015-10-28 10:47:08 +0000

    added a helper

--- spec/features/names_feature_spec.rb
+++ spec/features/names_feature_spec.rb
@@ -1,19 +1,13 @@
 feature 'Usernames' do
   scenario 'should be able to input and view player names' do
-    visit('/')
-    fill_in('player1', with: 'Andrew')
-    fill_in('player2', with: 'Mahmud')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content "Andrew vs Mahmud"
   end
 end
 
 feature 'Player health' do
   scenario 'Viewing Player 2\'s health' do
-    visit('/')
-    fill_in('player1', with: 'Andrew')
-    fill_in('player2', with: 'Mahmud')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content "Mahmud's health: 100"
   end
 end
