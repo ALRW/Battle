4348ec43f0259950b353e7c43c99749788be8796
alaanzr <alaanzr@gmail.com>
2015-10-28 10:34:21 +0000

    Added player 2 health

--- spec/features/names_feature_spec.rb
+++ spec/features/names_feature_spec.rb
@@ -1,9 +1,19 @@
-describe 'can go to index page' do
-  it'should be able to input player names' do
+feature 'Usernames' do
+  scenario 'should be able to input and view player names' do
     visit('/')
     fill_in('player1', with: 'Andrew')
     fill_in('player2', with: 'Mahmud')
     click_button('Submit')
     expect(page).to have_content "Andrew vs Mahmud"
+  end
+end
+
+feature 'Player health' do
+  scenario 'Viewing Player 2\'s health' do
+    visit('/')
+    fill_in('player1', with: 'Andrew')
+    fill_in('player2', with: 'Mahmud')
+    click_button('Submit')
+    expect(page).to have_content "Mahmud's health: 100"
   end
 end
